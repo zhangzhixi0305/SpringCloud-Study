d32144f4ae35b9110d71e245aeb69b2ff0af8e6a
zhangzhixi <1820712542@qq.com>
2023-04-06 23:07:33 +0800

    自定义Ribbon负载均衡策略

--- cloud-consumer-order-80/src/main/java/com/zhixi/OrderMain80.java
+++ cloud-consumer-order-80/src/main/java/com/zhixi/OrderMain80.java
@@ -1,8 +1,10 @@
 package com.zhixi;
 
+import com.zhixi.config.MyRibbonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @ClassName OrderMain80
@@ -13,6 +15,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+/**
+ * name: 指定要使用的Ribbon负载均衡策略的服务名称 configuration: 指定自定义的Ribbon负载均衡策略类
+ */
+@RibbonClient(name = "cloud-payment-service", configuration = MyRibbonConfig.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
---
+++ cloud-consumer-order-80/src/main/java/com/zhixi/config/MyRibbonConfig.java
@@ -0,0 +1,26 @@
+package com.zhixi.config;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName MyRibbonConfig
+ * @Author zhangzhixi
+ * @Description 自定义Ribbon负载均衡策略
+ * @Date 2023-04-06 22:03
+ * @Version 1.0
+ */
+@Configuration
+public class MyRibbonConfig {
+
+    /**
+     * 自定义负载均衡策略
+     * @return IRule
+     */
+    @Bean
+    public IRule myRule(){
+        return new RandomRule();
+    }
+}
